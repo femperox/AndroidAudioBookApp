6fded56a69f5a30e7337489bee4dd8d0a541e99f
Filchukova Daria <filchukova_de@magnit.ru>
2023-03-28 17:04:12 +0300

    Структурирование по папкам

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,13 +5,15 @@ import android.app.Fragment;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
+
+import com.example.myapplication.fragments.BookFragment;
+import com.example.myapplication.fragments.Recommendation;
+import com.example.myapplication.fragments.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
 
